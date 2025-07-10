d620814dcfcfa3121733ec607201e419e29c0508
Adam L Kirsch <akirsch@pinback.xray.aps.anl.gov>
2025-07-10 16:40:37 -0500

    trying chat template jinja file for LLaMA. Can't get around token limit issue for now

--- local_inference.sh
+++ local_inference.sh
@@ -3,10 +3,15 @@
 # Optional: choose a specific GPU
 export CUDA_VISIBLE_DEVICES=1
 
+# Verify the chat template
+echo "Starting VLLM with chat template: /home/beams/AKIRSCH/rareevent/vllm/chat-template.jinja"
+
+
 # Start the OpenAI-compatible VLLM server
 python -m vllm.entrypoints.openai.api_server \
   --model /home/beams/AKIRSCH/rareevent/vllm/Llama-2-13B-GPTQ \
   --port 8000 \
   --dtype auto \
   --host 0.0.0.0 \
+  --chat-template /home/beams/AKIRSCH/rareevent/vllm/chat-template.jinja
 #   --metrics-port 8001
